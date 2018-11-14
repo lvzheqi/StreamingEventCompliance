e1dbdd803b46066cd298207d712e91ba2e1e6e0f
lyu <lvzheqi@gmail.com>
2018-11-14 15:47:53 +0100

    update requirements

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/hhh')
+@app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
+@app.route('/compliance-checker', methods=['POST'])
+def register():
+    event = request.json
+    print('case_id: ', event['case_id'])
+    print('activity: ', event['activity'])
+    return 'deviation'
 
 
 if __name__ == '__main__':
