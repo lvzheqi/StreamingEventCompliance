3a7c411594bc26592e8692dc6b30cab197602c89
lyu <lvzheqi@gmail.com>
2018-11-20 01:23:51 +0100

    database-connection

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, request, send_file
 from services import complianceChecker, deviationPDF, buildAutomata
-from config import defaultConfig
+from utils import defaultConfig
+from test import dbTest
+from test.dbTest import db as testdb
+from objects.automata.automata import db as atdb
+from objects.automata.alertLog import db as altdb
 import json
 
 
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = defaultConfig.BASE_DIR
+app.config['SQLALCHEMY_DATABASE_URI'] = defaultConfig.DATABASE_PATH
 
+app.app_context().push()
+testdb.init_app(app)
+atdb.init_app(app)
+altdb.init_app(app)
 
 @app.route('/')
 def index():
@@ -28,19 +37,25 @@ def call_compliance_checker():
 
 @app.route('/show-deviation-pdf', methods=['GET', 'POST'])
 def call_show_deviation_pdf():
+    '''
+
+    :return:
+    '''
     client_uuid = request.args.get('uuid')
     deviationPDF.show_deviation_pdf(client_uuid)
-    print(defaultConfig.BASE_DIR)
     try:
-        automata_pdf = open(defaultConfig.AUTOMATA_FILE, 'rb')
+        automata_pdf = open(client_uuid + "-" + defaultConfig.AUTOMATA_FILE, 'rb')
         return send_file(automata_pdf, attachment_filename='file.pdf')
     except:
         print('exception')
 
 
-# TODO: catch exceptions
 buildAutomata.test_automata_status()
+# TODO: catch exceptions
 
+
+###### Test ######
+dbTest.init()
 
 if __name__ == '__main__':
     app.debug = True
