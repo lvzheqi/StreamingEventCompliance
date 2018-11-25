32d25b3ce5c73b7d6300c2c1b72c35c8acf7aeff
jingjinghuo <huo841022920@163.com>
2018-11-25 19:27:03 +0100

    Merge branch 'master' of https://github.com/lvzheqi/StreamingEventCompliance
    # Conflicts:
    #	client/simulate_stream_event/eventThread.py

---
+++ client/simulate_stream_event/eventthread.py
@@ -0,0 +1,23 @@
+from threading import Thread
+import requests
+
+
+class EventThread(Thread):
+
+    def __init__(self, event, client_uuid):
+        self.event = event
+        self.client_uuid = client_uuid
+        Thread.__init__(self)
+
+    def run(self):
+        try:
+            r = requests.post('http://127.0.0.1:5000/compliance-checker?uuid=' + self.client_uuid, json=self.event)
+            if r.status_code != 200:
+                print('Error: error by compliance checking')
+            else:
+                print("Info:", r.text)
+        except Exception:
+            print('The server is disconnect, please try again later')
+
+
+
