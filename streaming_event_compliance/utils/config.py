b7d2b28ddc557e1b153ca0eaad3518b639dc410b
sabyashaikh <sabya.shaikh@gmail.com>
2018-11-28 01:01:54 +0100

    Logging file added

--- streaming_event_compliance/utils/config.py
+++ streaming_event_compliance/utils/config.py
@@ -3,9 +3,10 @@ import os
 DATABASE_PATH = 'mysql+pymysql://compliancechecker:compliancechecker@localhost/compliancechecker'
 
 AUTOMATA_FILE = 'automata.pdf'
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(__file__)+ os.sep + '..' + os.sep + '..' + os.sep
 WINDOW_SIZE = [1, 2, 3, 4]
 
 
-TRAINING_EVENT_LOG_PATH = 'data/Example_EventLogForTraining.xes'
+TRAINING_EVENT_LOG_PATH = BASE_DIR + 'data'+os.sep+'Example_EventLogForTraining.xes'
 MAXIMUN_WINDOW_SIZE = '3'
+
