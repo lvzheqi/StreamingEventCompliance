e7fa00eca6b5271b65833adc43b6caae4b7888f7
lyu <lvzheqi@gmail.com>
2018-11-28 10:23:58 +0100

    bug fix

--- client/pm4py/visualization/common/__init__.py
+++ client/pm4py/visualization/common/__init__.py
@@ -1,1 +1,1 @@
-from streaming_event_compliance.pm4py.visualization.common import save, utils
+
--- client/pm4py/visualization/process_tree/factory.py
+++ client/pm4py/visualization/process_tree/factory.py
@@ -1,5 +1,4 @@
-from streaming_event_compliance.pm4py.visualization.common import gview
-from streaming_event_compliance.pm4py.visualization.common import save as gsave
+from pm4py.visualization.common import gview, save as gsave
 from pm4py.visualization.process_tree.versions import wo_decoration
 
 WO_DECORATION = "wo_decoration"
