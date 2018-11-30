084610a3e3bf02ea35851f3b3b34ea0b44d8de08
sabyashaikh <sabya.shaikh@gmail.com>
2018-11-30 14:32:01 +0100

    Merge branch 'master' of https://github.com/lvzheqi/StreamingEventCompliance
    # Conflicts:
    #	client/application.py
    #	client/client_log.log
    #	client/pm4py/algo/filtering/common/attributes/__init__.py
    #	client/pm4py/algo/filtering/dfg/__init__.py
    #	client/pm4py/objects/process_tree/__init__.py
    #	client/pm4py/visualization/__init__.py
    #	client/pm4py/visualization/common/__init__.py
    #	client/simulate_stream_event/client_logging.py

--- client/application.py
+++ client/application.py
@@ -1,6 +1,5 @@
 from simulate_stream_event import eventlog, client_logging
 from simulate_stream_event.exception import ReadFileException
-from pm4py.objects.log.importer.xes import factory as xes_importer
 from multiprocessing import Process
 import sys
 import requests
@@ -16,9 +15,9 @@ class Client_cls(object):
     def run_compliance_checker(self):
         func_name = sys._getframe().f_code.co_name
         try:
-            client_logging.client_logging(message_type="INFO", level="DEBUG", func_name=func_name, username=self.uuid,
-                                          message="Calling read_log()")
-            event_log = eventlog.read_log(self.uuid, self.path)
+             client_logging.client_logging(message_type="INFO", level="DEBUG", func_name=func_name, username=self.uuid,
+                                               message="Calling read_log()")
+             event_log = eventlog.read_log(self.uuid, self.path)
         except Exception:
             client_logging.client_logging(message_type="ERROR", level="DEBUG", func_name=func_name, username=self.uuid,
                                           message="Exception raised while reading file")
