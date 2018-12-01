18408dae840937860aa6123d57ac2b54352228e8
jingjinghuo <huo841022920@163.com>
2018-12-01 21:31:56 +0100

    small change about the exception_client

--- test/execute_tests.py
+++ test/execute_tests.py
@@ -3,7 +3,7 @@ import unittest
 if __name__ == '__main__':
     from test.database_test import DBToolsTest
     from test.automata_test import AutomataTest
-    from test.building_automata_test_case import BuildingAutomataTestCase
+    from test.building_automata_test import BuildingAutomataTestCase
 
     test_object1 = DBToolsTest()
     test_object2 = AutomataTest()
