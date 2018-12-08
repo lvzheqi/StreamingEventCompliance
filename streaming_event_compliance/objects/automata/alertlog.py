47642bbd301de17c08464f4aa20ab78a036969c6
lyu <lvzheqi@gmail.com>
2018-12-08 15:54:48 +0100

    add some functions for automata checking

--- streaming_event_compliance/objects/automata/alertlog.py
+++ streaming_event_compliance/objects/automata/alertlog.py
@@ -8,13 +8,16 @@ class AlertLog:
         self.uuid = uuid
         self.alert_log = []
 
-    def add_alert_record(self, alert_record):
+    def update_alert_record(self, alert_record):
         if alert_record in self.alert_log:
             index = self.alert_log.index(alert_record)
             alert = self.alert_log[index]
-            alert.alert_count += 1
+            alert.alert_count += alert_record.alert_count
         else:
             self.alert_log.append(alert_record)
+
+    def add_alert_record_from_database(self, alert_record):
+        self.alert_log.append(alert_record)
 
     def __repr__(self):
         return 'User name: %s' % self.uuid + '\n' + \
@@ -45,11 +48,12 @@ class AlertRecord(db.Model):
     alert_cause = db.Column('alert_cause', db.String(1))
     alert_count = db.Column('alert_count', db.Float)
 
-    def __init__(self, user_id, source_node, sink_node, alert_count):
+    def __init__(self, user_id, source_node, sink_node, alert_count=1, alert_cause='M'):
         self.user_id = user_id
         self.source_node = source_node
         self.sink_node = sink_node
         self.alert_count = alert_count
+        self.alert_cause = alert_cause
 
     def __eq__(self, other):
         return self.user_id == other.user_id and \
