d0944b02cf92528af84642ac2f3ee6d87812a3ec
lyu <lvzheqi@gmail.com>
2018-12-08 17:10:35 +0100

    bug fix

--- streaming_event_compliance/utils/config.py
+++ streaming_event_compliance/utils/config.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(__file__) + os.sep + '..' + os.sep + '..' + os.sep
 WINDOW_SIZE = [1, 2, 3, 4]
 
 
-TRAINING_EVENT_LOG_PATH = BASE_DIR + 'data' + os.sep + 'Example_EventLogForTraining.xes'
+TRAINING_EVENT_LOG_PATH = BASE_DIR + 'data' + os.sep + 'Example_EventLogForTraining_Backup.xes'
 AUTOS_DEFAULT = False
 MAXIMUN_WINDOW_SIZE = 4
 THRESHOLD = 0.00
