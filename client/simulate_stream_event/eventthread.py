ff0f9d682e4447f603913df85f810f26a5210418
sabyashaikh <sabya.shaikh@gmail.com>
2018-12-09 08:35:04 +0100

    changes to client logging class name and server logging class created

--- client/simulate_stream_event/eventthread.py
+++ client/simulate_stream_event/eventthread.py
@@ -2,7 +2,7 @@ from threading import Thread
 import requests
 import sys
 import json
-from .client_logging import Client_logging
+from .client_logging import ClientLogging
 from .exception import ConnectionException
 
 
@@ -25,12 +25,12 @@ class EventThread(Thread):
     def run(self):
         func_name = sys._getframe().f_code.co_name
         try:
-            Client_logging().log_info(func_name, self.client_uuid, self.index, self.event['case_id'], self.event['activity'],
+            ClientLogging().log_info(func_name, self.client_uuid, self.index, self.event['case_id'], self.event['activity'],
                                           "Posting event to server:http://127.0.0.1:5000/compliance-checker")
             r = requests.post('http://127.0.0.1:5000/compliance-checker?uuid=' + self.client_uuid,
                               json=json.dumps(self.event))
         except requests.exceptions.RequestException:
-            Client_logging().log_error(func_name, self.client_uuid, self.index, self.event['case_id'], self.event['activity'],
+            ClientLogging().log_error(func_name, self.client_uuid, self.index, self.event['case_id'], self.event['activity'],
                                      "The server got disconnected, please try again later ")
             print(ConnectionException.message)
         # TODO: jingjinghuo: i want to raise this exception, and break it in for-loop, but i cannot do it, why?
@@ -41,11 +41,11 @@ class EventThread(Thread):
         else: # request is successful
             if r.status_code != 200:
                 # TODO: jingjinghuo: Problem: What time this belowing code will be executed?
-                Client_logging().log_error(func_name, self.client_uuid, self.index, self.event['case_id'], self.event['activity'],
+                ClientLogging().log_error(func_name, self.client_uuid, self.index, self.event['case_id'], self.event['activity'],
                                               "Error by compliance checking")
                 print('Error: error by compliance checking')
             else:
-                Client_logging().log_info(func_name, self.client_uuid, self.index, self.event['case_id'], self.event['activity'],
+                ClientLogging().log_info(func_name, self.client_uuid, self.index, self.event['case_id'], self.event['activity'],
                                               "The server response is: "+r.text)
                 print("Info:", r.text)
 
