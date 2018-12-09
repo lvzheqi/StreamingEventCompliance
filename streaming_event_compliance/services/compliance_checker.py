2ccd53f85e1f631ef849d67eca7053da7f2fc33a
sabyashaikh <sabya.shaikh@gmail.com>
2018-12-09 06:40:01 +0100

    compliance_checker_v5

--- streaming_event_compliance/services/compliance_checker.py
+++ streaming_event_compliance/services/compliance_checker.py
@@ -2,7 +2,9 @@ from streaming_event_compliance.services import deviation_pdf
 from streaming_event_compliance.services.compliance_check import case_thread_cc
 from streaming_event_compliance.services.globalvar import ThreadMemorizer, CaseMemorizer
 from streaming_event_compliance.utils.config import MAXIMUN_WINDOW_SIZE
+from streaming_event_compliance.services.compliance_check.compare_automata import  alert_logs
 import threading
+import queue
 
 T = ThreadMemorizer()
 C = CaseMemorizer()
@@ -12,25 +14,29 @@ threads_index = 0
 
 def compliance_checker(client_uuid, event):
     '''
-    Does the compliance checking of the particular event received from client by comparing
-    the automata information in the database. Returns alerts, when some deviations occur.
+    This function creates a case_memorizer for each case_id and starts thread.
+    The threads return "deviation" message at the end if there is any deviation
+    for the event
     :param client_uuid: user name
     :param event: the event that we want to check the compliance
     :return:the deviation information or success information.
     '''
+    print(event['case_id'] + " " + event['activity'])
     global threads_index
     if event['case_id'] != 'NONE' and event['activity'] != 'END':
         if event['case_id'] in C.dictionary_cases:
+                # 1. Append the incoming event to already existing caseMemory of the with same case_id
+                # 2. Create a new thread for this case
+                # 3. Start it
                 C.dictionary_cases.get(event['case_id']).append(event['activity'])
                 thread = case_thread_cc.CaseThreadForCC(event, threads_index, T, C, client_uuid)
                 T.dictionary_threads[threads_index] = case_thread_cc
                 try:
                     thread.start()
-                    threads.append(case_thread_cc)  # this is for limiting the number of the threads that are runing???
+                    threads.append(case_thread_cc)
                     threads_index = threads_index + 1
                 except KeyboardInterrupt:
                     print('Error: Thread is interrupt!')
-                # Do we need restart the thread for this case?
         else:
                 # 1. Add it to the caseMemory
                 # 2. Create a new thread for this case
@@ -45,7 +51,7 @@ def compliance_checker(client_uuid, event):
                 # this is just for remember the threads information that we have ceated.
                 try:
                     thread.start()
-                    threads.append(case_thread_cc)  # this is for limiting the number of the threads that are runing???
+                    threads.append(case_thread_cc)
                     threads_index = threads_index + 1
                 except KeyboardInterrupt:
                     print('Error: Thread is interrupt!')
@@ -55,10 +61,14 @@ def compliance_checker(client_uuid, event):
          #  del threads[0]     # why we delete?
 
         # TODO: analyse and write non-compliance event to the database AlertLog with client_uuid
+     # TODO: Sabya Remove the below elif... This portion is only to show for now the content of alert logs...
+     # TODO: After inserting Alert_logs into table remove this below elif part
+    elif event['case_id'] == 'NONE' and event['activity'] == 'END':
+        print(alert_logs)
     else:
         deviation_pdf.build_deviation_pdf(client_uuid)
     # deviation information should be returned here, or we return it form thread.start()
-    return 'deviation'
+    return event['case_id'] + "->" + event['activity']
 
 
 def error_handle():
