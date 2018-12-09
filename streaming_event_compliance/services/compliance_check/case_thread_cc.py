7d8ef73633d36189307d6061adace486209efb74
sabyashaikh <sabya.shaikh@gmail.com>
2018-12-09 06:37:51 +0100

    compliance_checker_v4

--- streaming_event_compliance/services/compliance_check/case_thread_cc.py
+++ streaming_event_compliance/services/compliance_check/case_thread_cc.py
@@ -3,8 +3,6 @@ import time
 from streaming_event_compliance.utils.config import WINDOW_SIZE, MAXIMUN_WINDOW_SIZE
 from . import compare_automata
 
-check_executing_order = {}
-
 
 class CaseThreadForCC(Thread):
     def __init__(self, event, index, T, C, client_uuid):
@@ -17,19 +15,18 @@ class CaseThreadForCC(Thread):
 
     def run(self):
         """
-        in caseMemorizer every case we will memory the last 5 events that have been processed,
-        so for the current event processing event should in the 6. position? So after we processed
-        one event, we should delete the first one in the list. And if in the 6. position we don't
-        have event, that means currently all the events from this case has been processed.
-        This thread can do noting excepting waiting.
-        But during the processing the list will change, some events will be added into it,
-        how do we let the thread know that?
+        in caseMemorizer every case we will memory the last 4 events that have been processed,
+        so, the current processing event will be in the 5th  position. So after processing
+        one event, we delete the first one in the list. And if in the 5th position we don't
+        have any event, that means currently all the events from this case has been processed.
+        This function acquires lock on caseMemorizer and releases lock when processing of the
+        event is done
         """
         print(self, "Now ", time.time(), "the event ", self.event['activity'], "of case ",
               self.event['case_id'], "is started.")
-
         print("we are checking the status of lock for this event:",
               self.C.lock_List.get(self.event['case_id']))
+
         # Acquire the thread lock for current event
         self.C.lock_List.get(self.event['case_id']).acquire()
         print('case ', self.event['case_id'], 'is locked, because ', self.event['activity'],
@@ -41,17 +38,10 @@ class CaseThreadForCC(Thread):
         else:
             windows_memory = self.C.dictionary_cases.get(self.event['case_id'])[0: MAXIMUN_WINDOW_SIZE+1]
 
-        create_source_sink_node(windows_memory, self. client_uuid)
+        create_source_sink_node(windows_memory, self.client_uuid)
+
         if len(self.C.dictionary_cases.get(self.event['case_id'])) > MAXIMUN_WINDOW_SIZE:
             self.C.dictionary_cases.get(self.event['case_id']).pop(0)
-
-        '''--------For Testing: Before releasing lock, which thread used it will be stored-------'''
-        if check_executing_order.get(self.event['case_id']):
-            check_executing_order.get(self.event['case_id']).append(self.event['activity'])
-        else:
-            check_executing_order[self.event['case_id']] = []
-            check_executing_order[self.event['case_id']].append(self.event['activity'])
-        '''--------For Testing: Before releasing lock, which thread used it will be stored-------'''
 
         self.C.lock_List.get(self.event['case_id']).release()
 
@@ -65,22 +55,22 @@ def create_source_sink_node(windowsMemory, client_uuid):
     Create sink node and source node based on prefix sizes and call function to check compliance with automata in db
     :param windowsMemory: a list of activities from the same case_id of current event(another event),
                          size is maximum_window_size,
-                         and the current event is in the last position of the windowsMemory
+                         and the current event is at the last position of the windowsMemory
                          (i.e. event == windowsMemory[maximum_window_size])
     :return:
     """
     for ws in WINDOW_SIZE:# [1, 2, 3, 4]
-        #print(windowsMemory)
+        # print(windowsMemory)
         source_node = ','.join(windowsMemory[MAXIMUN_WINDOW_SIZE - ws: MAXIMUN_WINDOW_SIZE])
         sink_node = ','.join(windowsMemory[MAXIMUN_WINDOW_SIZE - ws + 1: MAXIMUN_WINDOW_SIZE+1])
         if source_node.find('*') == -1:
-            #print('WS: ' + str(ws) + ' Source: ' + source_node + ' Sink: ' + sink_node)
+            # print('WS: ' + str(ws) + ' Source: ' + source_node + ' Sink: ' + sink_node)
             matches = compare_automata.check_automata_with_source_sink(ws, source_node, sink_node, client_uuid)
             if matches == 0:
                 print("alert")
                 return "alert"
         elif source_node.find('*') != -1 and sink_node.find('*') == -1:
-            #print('WS: ' + str(ws) + ' Source: ' + source_node + ' Sink: ' + sink_node)
+            # print('WS: ' + str(ws) + ' Source: ' + source_node + ' Sink: ' + sink_node)
             matches = compare_automata.check_automata_only_sourcenode(ws, sink_node, client_uuid)
             if matches == 0:
                 print("alert")
