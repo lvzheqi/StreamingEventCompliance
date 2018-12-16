d70dc0e3a77235862ebea7a30aa6d9e154cadaeb
jingjinghuo <huo841022920@163.com>
2018-12-16 16:50:21 +0100

    j1

--- streaming_event_compliance/utils/config.py
+++ streaming_event_compliance/utils/config.py
@@ -9,6 +9,7 @@ MAXIMUN_WINDOW_SIZE = 4
 
 BASE_DIR = os.path.dirname(__file__) + os.sep + '..' + os.sep + '..' + os.sep
 TRAINING_EVENT_LOG_PATH = BASE_DIR + 'data' + os.sep + 'Example_EventLogForTraining.xes'
+# TRAINING_EVENT_LOG_PATH = BASE_DIR + 'data' + os.sep + 'Example_EventLogForTraining_Backup.xes'
 # TRAINING_EVENT_LOG_PATH = BASE_DIR + 'data' + os.sep + 'A21.xes'
 # TRAINING_EVENT_LOG_PATH = BASE_DIR + 'data' + os.sep + 'A1.xes'
 # TRAINING_EVENT_LOG_PATH = BASE_DIR + 'data' + os.sep + 'A4.xes'
