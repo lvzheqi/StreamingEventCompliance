08fbcf16565b6e2ec71927a6daff01c37cc26513
jingjinghuo <huo841022920@163.com>
2018-12-21 15:36:12 +0100

    j5

--- streaming_event_compliance/__init__.py
+++ streaming_event_compliance/__init__.py
@@ -25,7 +25,9 @@ app.config['CHECKING_TYPE'] = 'KEEP_ALL_EVENTS'
 app.config['SERVER_LOG_PATH'] = app.config['BASE_DIR'] + os.sep + 'data' + os.sep + 'server.log'
 app.config['ALERT_TYPE'] = 'RETURN_ONE'
 
-
+console.secure('DEFALT', 'WINDOW_SIZE: ' + str(app.config['WINDOW_SIZE']) + '\t'
+               'CHECKING_TYPE: ' + app.config['CHECKING_TYPE'] + '\t'
+               + 'ALERT_TYPE: ' + app.config['ALERT_TYPE'])
 config = configparser.ConfigParser()
 app.config['CONFIG_PATH'] = app.config['BASE_DIR'] + 'config.ini'
 config.read(app.config['CONFIG_PATH'])
@@ -46,11 +48,13 @@ if config['USER-DEFINED'].get('THRESHOLD') and config['USER-DEFINED']['THRESHOLD
 if config['USER-DEFINED'].get('CHECKING_TYPE') and config['USER-DEFINED']['CHECKING_TYPE'] is not '':
     app.config['CHECKING_TYPE'] = config['USER-DEFINED']['CHECKING_TYPE']
 
-if config['USER-DEFINED'].get('CHECKING_TYPE') and config['USER-DEFINED']['ALERT_TYPE'] is not '':
+if config['USER-DEFINED'].get('ALERT_TYPE') and config['USER-DEFINED']['ALERT_TYPE'] is not '':
     app.config['ALERT_TYPE'] = config['USER-DEFINED']['ALERT_TYPE']
 
 console.secure("PATH", app.config['TRAINING_EVENT_LOG_PATH'])
-
+console.secure('USER-DEFINED', 'WINDOW_SIZE: ' + str(app.config['WINDOW_SIZE']) + '\t'
+               'CHECKING_TYPE: ' + app.config['CHECKING_TYPE'] + '\t'
+               + 'ALERT_TYPE: ' + app.config['ALERT_TYPE'])
 db = SQLAlchemy(app)
 
 from streaming_event_compliance import routes
